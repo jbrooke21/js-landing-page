a15c83bf141449f25d1f2777355823795f1a9a37
jbrooke21 <jessica.brooke@symphony.com>
2016-05-24 15:26:24 -0700

    js updates

--- divided-times/js/app.js
+++ divided-times/js/app.js
@@ -1,131 +1,157 @@
-$(document).ready (function() {
-	
-	$(document).on("click", "li.international", function (event) {
-		event.preventDefault();
+$(document).ready(function() {
 
-		if ($(this).hasClass("active")) {
-			$(this).removeClass("active");
-			$("#slide-down").slideUp();
-		} else {
-			$("#primary-nav li").removeClass("active");
+  var $internationalDrop = $("#international-drop");
+  var $politicsDrop = $("#politics-drop");
+  var $businessDrop = $("#business-drop");
+  var $technologyDrop = $("#technology-drop");
+  var $cultureDrop = $("#culture-drop");
+  var $blogsDrop = $("blogs-drop");
 
-			$("#international-drop").show();
-			$("#politics-drop").hide();
-			$("#business-drop").hide();
-			$("#technology-drop").hide();
-			$("#culture-drop").hide();
-			$("#blogs-drop").hide();
-
-			$("#slide-down").slideDown()
-			$(this).addClass("active")
-		}
-	});
-
-	$(document).on("click", "li.politics", function (event) {
-		event.preventDefault();
-
-		if ($(this).hasClass("active")) {
-			$(this).removeClass("active");
-			$("#slide-down").slideUp();
-		} else {
-			$("#primary-nav li").removeClass("active");
-
-			$("#politics-drop").show();
-			$("#international-drop").hide();
-			$("#business-drop").hide();
-			$("#technology-drop").hide();
-			$("#culture-drop").hide();
-			$("#blogs-drop").hide();
-
-			$("#slide-down").slideDown()
-			$(this).addClass("active")
-		}
-	});
-
-	$(document).on("click", "li.business", function (event) {
-		event.preventDefault();
-
-		if ($(this).hasClass("active")) {
-			$(this).removeClass("active");
-			$("#slide-down").slideUp();
-		} else {
-			$("#primary-nav li").removeClass("active");
-
-			$("#business-drop").show();
-			$("#international-drop").hide();
-			$("#politics-drop").hide();
-			$("#technology-drop").hide();
-			$("#culture-drop").hide();
-			$("#blogs-drop").hide();
-
-			$("#slide-down").slideDown()
-			$(this).addClass("active")
-		}
-	});
-
-	$(document).on("click", "li.technology", function (event) {
-		event.preventDefault();
-
-		if ($(this).hasClass("active")) {
-			$(this).removeClass("active");
-			$("#slide-down").slideUp();
-		} else {
-			$("#primary-nav li").removeClass("active");
-
-			$("#technology-drop").show();
-			$("#business-drop").hide();
-			$("#international-drop").hide();
-			$("#politics-drop").hide();
-			$("#culture-drop").hide();
-			$("#blogs-drop").hide();
-
-			$("#slide-down").slideDown()
-			$(this).addClass("active")
-		}
-	});
-
-	$(document).on("click", "li.culture", function (event) {
-		event.preventDefault();
-
-		if ($(this).hasClass("active")) {
-			$(this).removeClass("active");
-			$("#slide-down").slideUp();
-		} else {
-			$("#primary-nav li").removeClass("active");
-
-			$("#culture-drop").show();
-			$("#technology-drop").hide();
-			$("#business-drop").hide();
-			$("#international-drop").hide();
-			$("#politics-drop").hide();
-			$("#blogs-drop").hide();
-
-			$("#slide-down").slideDown()
-			$(this).addClass("active")
-		}
-	});
-
-	$(document).on("click", "li.blogs", function (event) {
-		event.preventDefault();
-
-		if ($(this).hasClass("active")) {
-			$(this).removeClass("active");
-			$("#slide-down").slideUp();
-		} else {
-			$("#primary-nav li").removeClass("active");
-			
-			$("#blogs-drop").show();
-			$("#culture-drop").hide();
-			$("#technology-drop").hide();
-			$("#business-drop").hide();
-			$("#international-drop").hide();
-			$("#politics-drop").hide();
-
-			$("#slide-down").slideDown()
-			$(this).addClass("active")
-		}
-	});
+  function hide () {
+      $("#international-drop").hide();
+      $("#politics-drop").hide();
+      $("#business-drop").hide();
+      $("#technology-drop").hide();
+      $("#culture-drop").hide();
+      $("#blogs-drop").hide();
+  }
+  function show () {
+      $("#international-drop").show();
+      $("#politics-drop").show();
+      $("#business-drop").show();
+      $("#technology-drop").show();
+      $("#culture-drop").show();
+      $("#blogs-drop").show();
+  }
 
 
+
+  $(document).on("click", "li.international", function(event) {
+    event.preventDefault();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $("#slide-down").slideUp();
+    } else {
+      $("#primary-nav li").removeClass("active");
+      $(this).addClass("active");
+
+      $internationalDrop.show();
+
+      $("#international-drop").show();
+      $("#politics-drop").hide();
+      $("#business-drop").hide();
+      $("#technology-drop").hide();
+      $("#culture-drop").hide();
+      $("#blogs-drop").hide();
+
+      $("#slide-down").slideDown();
+    }
+  });
+
+  $(document).on("click", "li.politics", function(event) {
+    event.preventDefault();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $("#slide-down").slideUp();
+    } else {
+      $("#primary-nav li").removeClass("active");
+      $(this).addClass("active");
+
+      $("#international-drop").hide();
+      $("#politics-drop").show();
+      $("#business-drop").hide();
+      $("#technology-drop").hide();
+      $("#culture-drop").hide();
+      $("#blogs-drop").hide();
+
+      $("#slide-down").slideDown();
+    }
+  });
+
+  $(document).on("click", "li.business", function(event) {
+    event.preventDefault();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $("#slide-down").slideUp();
+    } else {
+      $("#primary-nav li").removeClass("active");
+      $(this).addClass("active");
+
+      $("#international-drop").hide();
+      $("#politics-drop").hide();
+      $("#business-drop").show();
+      $("#technology-drop").hide();
+      $("#culture-drop").hide();
+      $("#blogs-drop").hide();
+
+      $("#slide-down").slideDown();
+    }
+  });
+
+  $(document).on("click", "li.technology", function(event) {
+    event.preventDefault();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $("#slide-down").slideUp();
+    } else {
+      $("#primary-nav li").removeClass("active");
+      $(this).addClass("active");
+
+      $("#international-drop").hide();
+      $("#politics-drop").hide();
+      $("#business-drop").hide();
+      $("#technology-drop").show();
+      $("#culture-drop").hide();
+      $("#blogs-drop").hide();
+
+      $("#slide-down").slideDown();
+    }
+  });
+
+  $(document).on("click", "li.culture", function(event) {
+    event.preventDefault();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $("#slide-down").slideUp();
+    } else {
+      $("#primary-nav li").removeClass("active");
+      $(this).addClass("active");
+
+      $("#international-drop").hide();
+      $("#politics-drop").hide();
+      $("#business-drop").hide();
+      $("#technology-drop").hide();
+      $("#culture-drop").show();
+      $("#blogs-drop").hide();
+
+      $("#slide-down").slideDown();
+    }
+  });
+
+  $(document).on("click", "li.blogs", function(event) {
+    event.preventDefault();
+
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $("#slide-down").slideUp();
+    } else {
+      $("#primary-nav li").removeClass("active");
+      $(this).addClass("active");
+
+      $("#international-drop").hide();
+      $("#politics-drop").hide();
+      $("#business-drop").hide();
+      $("#technology-drop").hide();
+      $("#culture-drop").hide();
+      $("#blogs-drop").show();
+
+      $("#slide-down").slideDown();
+    }
+  });
 
 });
